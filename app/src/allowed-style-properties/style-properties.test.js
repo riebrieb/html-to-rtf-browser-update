3152dfbdbdad686269058176e3b54f45f9e5224b
Heron Silva <code.heron@outlook.com>
2018-04-11 18:58:05 -0300

    Updating tests

--- app/src/allowed-style-properties/style-properties.test.js
+++ app/src/allowed-style-properties/style-properties.test.js
@@ -1,22 +1,30 @@
-const should          = require('should');
+const should = require('should');
 const StyleProperties = require('./style-properties.module');
 
 describe('StylePropertiesTest', () => {
-  it('Length of module', () => {
-    should(StyleProperties.length).be.equal(4);
-  });
-  
-  StyleProperties.forEach(element => {
-    if(element.propertyName == 'color')
-      it('color', () => should(element.allowed).be.true());
+    it('Length of module', () => {
+        should(StyleProperties.length).be.equal(5);
+    });
 
-    else if(element.propertyName == 'font-size')
-      it('font-size', () => should(element.allowed).be.true());
+    StyleProperties.forEach(element => {
+        if (element.propertyName == 'color') {
+            it('color', () => should(element.allowed).be.true());
+        }
 
-    else if(element.propertyName == 'text-align')
-      it('text-align', () => should(element.allowed).be.true());
+        else if (element.propertyName == 'font-size') {
+            it('font-size', () => should(element.allowed).be.true());
+        }
 
-    else if(element.propertyName == 'background')
-      it('background', () => should(element.allowed).be.false());
-  });
+        else if (element.propertyName == 'text-align') {
+            it('text-align', () => should(element.allowed).be.true());
+        }
+
+        else if (element.propertyName == 'background') {
+            it('background', () => should(element.allowed).be.false());
+        }
+
+        else if (element.propertyName == 'font-family') {
+            it('font-family', () => should(element.allowed).be.true());
+        }
+    });
 });
