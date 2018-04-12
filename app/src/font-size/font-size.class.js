b6cb6ffae3e8bf9844a28a3522dc0a8a6ea30e0d
Heron Silva <code.heron@outlook.com>
2018-04-12 15:22:06 -0300

    Add function to return the font-size in 'pt'

--- app/src/font-size/font-size.class.js
+++ app/src/font-size/font-size.class.js
@@ -2,15 +2,25 @@ const FONT_SIZE_RTF_REFERENCE = '\\fs';
 const ONE_PIXEL_IN_POINT = 0.75;
 
 class FontSize {
-  static getRtfFontSizeReference(value) {
-    if(value.includes('px'))
-      return this.getFontSizeReferenceInPx(value);
-      
-    return undefined;
-  }
+    static getRtfFontSizeReference(value) {
+        if (value.includes('px')) {
+            return this.getFontSizeReferenceInPx(value);
+        }
 
-  static getFontSizeReferenceInPx(valueInPixel) {
-    return FONT_SIZE_RTF_REFERENCE + Math.trunc(parseFloat(valueInPixel) * ONE_PIXEL_IN_POINT);
-  }
+        if (value.includes('pt')) {
+            return this.getFontSizeReferenceInPt(value);
+        }
+
+        return undefined;
+    }
+
+    static getFontSizeReferenceInPx(valueInPixel) {
+        return FONT_SIZE_RTF_REFERENCE + Math.trunc(parseFloat(valueInPixel) * ONE_PIXEL_IN_POINT);
+    }
+
+    static getFontSizeReferenceInPt(valueInPoints) {
+        return FONT_SIZE_RTF_REFERENCE + Math.trunc(parseFloat(valueInPoints) * 2);
+    }
 }
+
 module.exports = FontSize;
